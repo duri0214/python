ed98d3e204a2ba15940efca00e89370ef44d5d70
yoshi <yoshitakaOkada0214@gmail.com>
2023-04-12 23:07:30 +0900

    test ok

--- Ddd/tests/domain/repositories/test_monetary.py
+++ Ddd/tests/domain/repositories/test_monetary.py
@@ -21,13 +21,13 @@ class TestMonetaryRepository(TestCase):
         self.assertEqual(self.repository.data[1], monetary2)
 
     def test_filter_sum(self):
-        monetary1 = MonetaryEntity(100, CurrencyEntity("USD"))
+        monetary1 = MonetaryEntity(1000, CurrencyEntity("USD"))
         monetary2 = MonetaryEntity(200, CurrencyEntity("USD"))
-        monetary3 = MonetaryEntity(300, CurrencyEntity("JPY"))
+        monetary3 = MonetaryEntity(3000, CurrencyEntity("JPY"))
 
         self.repository.add(monetary1)
         self.repository.add(monetary2)
         self.repository.add(monetary3)
 
-        self.assertEqual(self.repository.filter_sum(CurrencyEntity("USD")), "300.0USD")
-        self.assertEqual(self.repository.filter_sum(CurrencyEntity("JPY")), "300.0JPY")
+        self.assertEqual(self.repository.filter_sum(CurrencyEntity("USD")), "1,200.0USD")
+        self.assertEqual(self.repository.filter_sum(CurrencyEntity("JPY")), "3,000.0JPY")
