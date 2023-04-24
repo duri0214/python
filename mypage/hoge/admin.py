81be77d28168b2dca54ee701828098fc6b69f439
yoshi <yoshitakaOkada0214@gmail.com>
2023-04-24 20:32:22 +0900

    delete

--- mypage/hoge/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Staff, Store, Schedule
-admin.site.register([Staff, Store, Schedule])
--- mypage/hoge/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HogeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hoge'
