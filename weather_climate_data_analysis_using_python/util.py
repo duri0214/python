3e9d72f2cb806aa3b84be69db3eabcacd886590f
yoshi <yoshitakaOkada0214@gmail.com>
2024-07-18 19:38:51 +0900

    valueobject update

--- weather_climate_data_analysis_using_python/util.py
+++ weather_climate_data_analysis_using_python/util.py
@@ -3,22 +3,20 @@ from pathlib import Path
 import numpy as np
 
 from weather_climate_data_analysis_using_python.valueobject.wcdaup import (
-    SeaSurfaceTemperature,
+    SeaSurfaceTemperatureData,
 )
 
 
-def get_sea_surface_temperature_data() -> SeaSurfaceTemperature:
+def get_sea_surface_temperature_data() -> SeaSurfaceTemperatureData:
     """
     2.2.3 海面水温ファイルを入力する
-    2.3.1 経度(longitude 0～360)と緯度(latitude 90～-90)
-      経度は東経0～180度、西経0～180度 でぜんぶで360要素(ただし、lon2は東に向かって0～360で定義される)
-      緯度は北緯0～90度、南緯0～90度、で全部で180要素(ただし、lat2は南緯が-90～0で定義される)
-      lon2, lat2 は360行180列の行列
+    2.3.1 経度(longitude)と緯度(latitude)
+    3.4.1 データセットの読み込み
     """
     file_path = Path("output") / "sst_OISST.npz"
     sst_dataset = np.load(file_path)
 
-    return SeaSurfaceTemperature(
+    return SeaSurfaceTemperatureData(
         sst=sst_dataset["sst"],
         lon2=sst_dataset["lon2"],
         lat2=sst_dataset["lat2"],
--- weather_climate_data_analysis_using_python/valueobject/wcdaup.py
+++ weather_climate_data_analysis_using_python/valueobject/wcdaup.py
@@ -4,7 +4,18 @@ import numpy
 
 
 @dataclass
-class SeaSurfaceTemperature:
+class SeaSurfaceTemperatureData:
+    """
+    Class representing sea surface temperature data.
+
+    Attributes:
+        sst (numpy.ndarray): Sea surface temperature data.
+        lon2 (numpy.ndarray): 経度は東経0～180度、西経0～180度 でぜんぶで360要素(ただし、lon2は東に向かって0～360で定義される)
+        lat2 (numpy.ndarray): 緯度は北緯0～90度、南緯0～90度、で全部で180要素(ただし、lat2は南緯が-90～0で定義される)
+        y (numpy.ndarray): Years.
+        m (numpy.ndarray): Months.
+    """
+
     sst: numpy.ndarray
     lon2: numpy.ndarray
     lat2: numpy.ndarray
